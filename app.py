15b21e0242c0e4cbf86abae627eb281c585d8d8b
KingHacker9000 <Coolioboss7@gmail.com>
2022-02-26 11:55:20 +0400

    Final

--- app.py
+++ app.py
@@ -16,6 +16,21 @@ def index():
     return render_template("home.html")
 
 
+@app.route("/glass")
+def glass():
+    return render_template("glass.html")
+
+@app.route("/neuralink")
+def neuralink():
+    return render_template("link.html")
+
+@app.route("/nanobots")
+def nanobots():
+    return render_template("nano.html")
+
+@app.route("/olfactorySenses")
+def olfactory():
+    return render_template("olfactory.html")
 
 # Run The Application
 if __name__ == "__main__":
